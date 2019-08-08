89b10de54199a3b2b2d9fc676252ea6e144280ba
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-08-08 14:07:51 -0400

    2nd Phase Commit

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,14 +1,32 @@
-//import java.util.Date;
+
 public class BlockChain 
 {
+	
+	//Properties
 	private String hash;
 	private String data;
-	
+	private String previousHash;
 
+	//constructor with only data is created
 public BlockChain(String data)
 {
 	this.data=data;
 	this.hash=hash;
+}
+//New constructor is created for calclulating previous hash
+public BlockChain(String data,String previousHash)
+{
+	this.data=data;
+	this.hash=hash;
+	this.previousHash=previousHash;
+}
+//getter and setters are generated
+public String getPreviousHash() {
+	return previousHash;
+}
+
+public void setPreviousHash(String previousHash) {
+	this.previousHash = previousHash;
 }
 
 public String getHash() {
@@ -27,9 +45,17 @@ public void setData(String data) {
 	this.data = data;
 }
 
+//Method for calclulating hash is generated as defined in phase1
 public int CalculateHash()
 {
 	int CalculatedHash=data.hashCode();
 	return CalculatedHash;
+}
+
+//Method for calculating previous hash is generated as mentioned in phase2
+public int CalculatePreviousHash()
+{
+	int CalculatePreviousHash=(data+previousHash).hashCode();
+	return CalculatePreviousHash;	
 }
 }
--- src/BlockChainMain.java
+++ src/BlockChainMain.java
@@ -3,10 +3,36 @@ public class BlockChainMain {
 
 	public static void main(String[] args) 
 	{
+		//first block in which data is passed and hash is calculated
+		
 		BlockChain MyFirstBlock=new BlockChain("ThisIsMyFirstBlock");
 		System.out.println("This is my first block: " +MyFirstBlock.CalculateHash());
-		MyFirstBlock.setData("ThisIsMyybhbhData");
+		
+		//data is changed and hash also changes when data changes
+		
+		MyFirstBlock.setData("ThisIsMyChangedData");
 		System.out.println("This is my changed hash code: "+MyFirstBlock.CalculateHash());
+		
+		//series of 4 blocks in which first one have data and all other are null
+		//the hash of secound one depend on first one hash and similar thing follow up for all blocks further
+
+		BlockChain MySecoundBlock=new BlockChain("ThisIsMyFirstBlock",MyFirstBlock.getHash());
+		System.out.println("This is my Secound block: " +MySecoundBlock.CalculatePreviousHash());
+		
+
+		BlockChain MyThirdBlock=new BlockChain(null,MySecoundBlock.getHash());
+		System.out.println("This is my Third block: " +MyThirdBlock.CalculatePreviousHash());
+		
+
+		BlockChain MyFourthBlock=new BlockChain(null,MyThirdBlock.getHash());
+		System.out.println("This is my fourth block: " +MyFourthBlock.CalculatePreviousHash());
+		
+
+		BlockChain MyFifthBlock=new BlockChain(null,MyFourthBlock.getHash());
+		System.out.println("This is my fifth block: " +MyFifthBlock.CalculatePreviousHash());
+		
+		
+		
 		
 	}
 }
