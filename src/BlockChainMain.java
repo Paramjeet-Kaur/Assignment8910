24f0352f8335325d44dcf18797fb0df9fc5b83e1
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-08-08 15:24:08 -0400

    Phase 3 Committ

--- src/BlockChainMain.java
+++ src/BlockChainMain.java
@@ -1,38 +1,52 @@
-
+import java.util.Scanner;
 public class BlockChainMain {
 
 	public static void main(String[] args) 
 	{
 		//first block in which data is passed and hash is calculated
 		
-		BlockChain MyFirstBlock=new BlockChain("ThisIsMyFirstBlock");
-		System.out.println("This is my first block: " +MyFirstBlock.CalculateHash());
+		BlockChain MyGenesisBlock=new BlockChain("ThisIsMyGenesisBlock");
+		System.out.println("This is my Genesis block: " +MyGenesisBlock.CalculateHash());
 		
 		//data is changed and hash also changes when data changes
-		
-		MyFirstBlock.setData("ThisIsMyChangedData");
-		System.out.println("This is my changed hash code: "+MyFirstBlock.CalculateHash());
+		MyGenesisBlock.setData("ThisIsMyChangedData");
+		System.out.println("This is my changed hash code: "+MyGenesisBlock.CalculateHash());
 		
 		//series of 4 blocks in which first one have data and all other are null
 		//the hash of secound one depend on first one hash and similar thing follow up for all blocks Further
 
-		BlockChain MySecoundBlock=new BlockChain("ThisIsMyFirstBlock",MyFirstBlock.getHash());
+		System.out.println("");
+		BlockChain MyFirstBlock=new BlockChain("ThisIsMyFirstBlock",MyGenesisBlock.getHash());
+		System.out.println("This is my First block: " +MyFirstBlock.CalculatePreviousHash());
+
+		System.out.println("");
+		BlockChain MySecoundBlock=new BlockChain(null,MyFirstBlock.getHash());
 		System.out.println("This is my Secound block: " +MySecoundBlock.CalculatePreviousHash());
 		
-
+		//Scanner is used for allowing user to enter data as mentioned in phase3
+		Scanner Keyboard=new Scanner(System.in);
+		System.out.println("Enter the data you want to store?");
+		String EnteredData=Keyboard.nextLine();
+		MySecoundBlock.setData(EnteredData);
+		System.out.println("My new Data is: "+EnteredData+ " : " +MySecoundBlock.CalculatePreviousHash());
+		
+	
+		System.out.println("");
 		BlockChain MyThirdBlock=new BlockChain(null,MySecoundBlock.getHash());
 		System.out.println("This is my Third block: " +MyThirdBlock.CalculatePreviousHash());
+		System.out.println("Enter the data you want to store?");
+		String EnteredData1=Keyboard.nextLine();
+		MyThirdBlock.setData(EnteredData1);
+		System.out.println("My new Data is: "+EnteredData1+ " : " +MyThirdBlock.CalculatePreviousHash());
 		
-
+		
+		System.out.println("");
 		BlockChain MyFourthBlock=new BlockChain(null,MyThirdBlock.getHash());
 		System.out.println("This is my fourth block: " +MyFourthBlock.CalculatePreviousHash());
-		
-
-		BlockChain MyFifthBlock=new BlockChain(null,MyFourthBlock.getHash());
-		System.out.println("This is my fifth block: " +MyFifthBlock.CalculatePreviousHash());
-		
-		
-		
-		
+		System.out.println("Enter the data you want to store?");
+		String EnteredData2=Keyboard.nextLine();
+		MyFourthBlock.setData(EnteredData2);
+		System.out.println("My new Data is: "+EnteredData2+ " : " +MyFourthBlock.CalculatePreviousHash());
+			
 	}
 }
